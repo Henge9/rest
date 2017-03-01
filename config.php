566fa140d817b02a5691a9216c8ec9e0be771fe6
Henge9 <perhenric@gmail.com>
2017-03-01 21:11:27 +0100

    worked on API

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $db_username = "root";
 $db_password = "";
 $db_host = "localhost";
-$db_database = "219310-abg"
+$db_database = "219310-abg";
 
 $db = mysqli_connect($db_host, $db_username, $db_password, $db_database);
  ?>
--- index.php
+++ index.php
@@ -2,47 +2,51 @@
 include_once 'config.php';
 
 
-echo "<pre>";
-$allowed_fields = ['name','phone','email','pincode'];
-// name=a&phone=b&email=c&pincode=d&gender=e
-foreach($_POST as $k => $v){
-	if(in_array($k, $allowed_fields)){
-		$fields[] = $k;
-		$value = mysqli_real_escape_string($db, $v);
-		$values[] = "'$value'";
-	}
+//writes the url /rest/tablename/column/id into $fullurl
+$fullurl = $_SERVER['REQUEST_URI']; 
+echo $fullurl;
+echo '<br>';
+//makes a array of the url
+$url_parts = explode('/', $fullurl);
+//split the aray up for easyer understanding of the code
+$tablename = $url_parts[2];
+//$columns = $url_parts[3];
+//$column_sort = $url_parts[4];
+//$value = $url_parts[5];
+
+/*=============================
+Please note that the request method 
+shall not be confused with $_POST and $_GET from 
+forms in HTML....
+HTTP:	|CRUD:	|mySQL:
+GET		|READ	|SELECT
+PUT		|UPDATE	|UPDATE
+POST	|CREATE	|INSERT INTO
+DELETE	|DELETE	|DELETE
+===============================*/
+$method = $_SERVER['REQUEST_METHOD'];
+
+//switch to make correct SQL query
+switch ($method) {
+	/*==========================
+	GET example:
+	/rest/tablename
+	
+	============================*/
+	case 'GET':
+		$query = 
+			"SELECT 'columns'
+			FROM $tablename
+			WHERE 'column_sort value'";
+		echo $query;
+		break;
+	
+	default:
+		echo "crap";
+		break;
 }
-$sql_fields = implode(',',$fields);
-$sql_values = implode(',',$values);
-echo "
-INSERT INTO users
-($sql_fields)
-VALUES
-($sql_values)
-";
-// ================================
-foreach($_POST as $k => $v){
-	if(in_array($k, $allowed_fields) && $v != ''){
-		$value = mysqli_real_escape_string($db, $v);
-		$keyvalues[] = "$k = '$value'";
-	}
-}
-$sql_keyvalues = implode(',',$keyvalues);
-echo "
-UPDATE users
-SET $sql_keyvalues
-";
-
-?>
+echo $method;
+/*
 
 
-<form method="post">
-name <input type="text" name='name'><br>
-phone <input type="text" name='phone'><br>
-email <input type="text" name='email'><br>
-pincode <input type="text" name='pincode'>
-<br>
-gender <input type="text" name='gender'>
-<input type='submit'>
-</form>
-
+*/
